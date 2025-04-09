3b1603639e731ea0c7159e77afdfa8ff46a4ae71
Areeba Memon <103986750+AreebaMemon@users.noreply.github.com>
2025-04-09 18:29:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+function updateClock() {
+    const d = new Date();
+    const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    const pakistanOffset = 5 * 60 * 60000; 
+    const pakTime = new Date(utc + pakistanOffset);
+    
+    const htime = pakTime.getHours();
+    const mtime = pakTime.getMinutes();
+    const stime = pakTime.getSeconds();
+    
+    
+    const hrotation = 30 * (htime % 12) + mtime / 2; 
+    const mrotation = 6 * mtime; 
+    const srotation = 6 * stime; 
 
+   
+    document.getElementById('hour').style.transform = `rotate(${hrotation}deg)`;
+    document.getElementById('minute').style.transform = `rotate(${mrotation}deg)`;
+    document.getElementById('second').style.transform = `rotate(${srotation}deg)`;
+}
 
-setInterval(() => {
-    d=new Date();
-    htime=d.getHours();
-    mtime=d.getMinutes();
-    stime=d.getSeconds();
-    hrotation=30*htime+mtime/2;
-    mrotation=60*mtime;
-    srotation=6*stime;
-
-    hour.style.transform=`rotate(${hrotation}deg)`;
-    minute.style.transform=`rotate(${mrotation}deg)`;
-    second.style.transform=`rotate(${srotation}deg)`;
-}, 1000);
+updateClock();
+setInterval(updateClock, 1000);
